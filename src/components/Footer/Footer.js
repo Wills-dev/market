97e3631a2eff50175dfc0891da8a12cb1457c7bc
Aysha Muhammed <64032658+Aysha-py@users.noreply.github.com>
2022-12-22 08:36:22 +0100

    All product page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,19 +6,57 @@ import { Iconly } from "react-iconly";
 const Footer = () => {
   return (
     <>
-    <footer>
-      <div className="footer-btm">
-        <div className="container">
-          <div className="fb-wrap">
-            <p className="copyright">©2022 Traders of Africa. All Rights Reserved</p>
-            <ul>
-              <li><Link>Terms & Conditions</Link></li>
-              <li><Link>Privacy Policy</Link></li>
-            </ul>
+      <footer>
+        <div className="footer-list">
+          <div className="Products">
+            <h5>Products</h5>
+
+            <Link to="">
+              <li>Home</li>
+            </Link>
+            <Link to="">
+              <li>Browse Products</li>
+            </Link>
+          </div>
+          <div className="needHelp">
+            <h5>Need Help</h5>
+
+            <Link to="">
+              <li>Contact Us</li>
+            </Link>
+
+            <Link to="">
+              <li>FAQ</li>
+            </Link>
+          </div>
+          <div className="footersubscription">
+            <h5>Subscription</h5>
+
+            <Link to="">
+              <li>Plans and Pricing</li>
+            </Link>
+          </div>
+          <div className="sellers">
+            <h5>Sellers</h5>
+
+            <Link to="">
+              <li>Become a seller</li>
+            </Link>
+            <Link to="">
+              <li>RFQ's</li>
+            </Link>
           </div>
         </div>
-      </div>
-    </footer>
+        <div className="footer-btm">
+          <div className="container">
+            <div className="">
+              <p className="copyright">
+                ©2022 Traders of Africa. All Rights Reserved
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
