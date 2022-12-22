78f4e355b00fb81d9b31d521e63ffcd7d6b2aacb
Aysha Muhammed <64032658+Aysha-py@users.noreply.github.com>
2022-12-22 18:10:03 +0100

    seller landing page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import ForgotPassword from "./pages/Authentication/ForgotPassword";
 import Login from "./pages/Authentication/Login";
 import ResetPassword from "./pages/Authentication/ResetPassword";
 import ProductListing from "./pages/ProductListing/ProductListing";
-import ProductListingInfo from "./pages/ProductListing/ProductListingInfo";
 
 import BuyersDashboard from "./pages/Dashboard/BuyersDashboard/BuyersDashboard";
 import BuyersMessageCenter from "./pages/Dashboard/BuyersDashboard/BuyersMessageCenter";
@@ -21,6 +20,7 @@ import BuyersOrderInfo from "./pages/Dashboard/BuyersDashboard/BuyersOrderInfo";
 import BuyersRfqQuote from "./pages/Dashboard/BuyersDashboard/BuyersRfqQuote";
 import BuyersRfqs from "./pages/Dashboard/BuyersDashboard/BuyersRfqs";
 import BuyersSettings from "./pages/Dashboard/BuyersDashboard/BuyersSettings";
+import BuyersRecievedQuote from "./pages/Dashboard/BuyersDashboard/BuyersRecievedQuote";
 
 import SellersDashboard from "./pages/Dashboard/SellersDashboard/SellersDashboard";
 import SellersMessageCenter from "./pages/Dashboard/SellersDashboard/SellersMessageCenter";
@@ -30,10 +30,10 @@ import SellersProducts from "./pages/Dashboard/SellersDashboard/SellersProducts"
 import SellersNewProduct from "./pages/Dashboard/SellersDashboard/SellersNewProduct";
 import SellersRfqs from "./pages/Dashboard/SellersDashboard/SellersRfqs";
 import SellersSettings from "./pages/Dashboard/SellersDashboard/SellersSettings";
-import SellersRfqInfo from "./pages/Dashboard/SellersDashboard/SellersRfqInfo"
-import SellersRfqsQuote from "./pages/Dashboard/SellersDashboard/SellersRfqsQuote"
+import SellersRfqInfo from "./pages/Dashboard/SellersDashboard/SellersRfqInfo";
+import SellersRfqsQuote from "./pages/Dashboard/SellersDashboard/SellersRfqsQuote";
 import SellersProductDescription from "./pages/Dashboard/SellersDashboard/SellersProductDescription";
-import BuyersRecievedQuote from "./pages/Dashboard/BuyersDashboard/BuyersRecievedQuote";
+
 
 
 function App() {
@@ -43,27 +43,45 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/all-product" element={<ProductListing />} />
-          <Route path="/product-listing-info" element={<ProductListingInfo />} />
+
           <Route path="/authentication" element={<Authentication />} />
           <Route path="/buyers-registration" element={<BuyersRegistration />} />
-          <Route path="/sellers-registration"  element={<SellersRegistration />} />
-          <Route path="/sellers-landing-page" element={<SellersLandingPage />} />
+          <Route
+            path="/sellers-registration"
+            element={<SellersRegistration />}
+          />
+          <Route
+            path="/sellers-landing-page"
+            element={<SellersLandingPage />}
+          />
           <Route path="/phone-verification" element={<PhoneVerification />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:resetToken" element={<ResetPassword />} />
+          <Route
+            path="/reset-password/:resetToken"
+            element={<ResetPassword />}
+          />
           <Route path="/login" element={<Login />} />
 
           <Route path="/buyer/dashboard" element={<BuyersDashboard />} />
-          <Route path="/buyer/message-center" element={<BuyersMessageCenter />} />
+          <Route
+            path="/buyer/message-center"
+            element={<BuyersMessageCenter />}
+          />
           <Route path="/buyer/orders" element={<BuyersOrders />} />
           <Route path="/buyer/order-info" element={<BuyersOrderInfo />} />
           <Route path="/buyer/quotes" element={<BuyersRfqQuote />} />
           <Route path="/buyer/rfqs" element={<BuyersRfqs />} />
-          <Route path="/buyer/recieved-quotes" element={<BuyersRecievedQuote />} />
+          <Route
+            path="/buyer/recieved-quotes"
+            element={<BuyersRecievedQuote />}
+          />
           <Route path="/buyer/settings" element={<BuyersSettings />} />
-          
+
           <Route path="/seller/dashboard" element={<SellersDashboard />} />
-          <Route path="/seller/message-center" element={<SellersMessageCenter />} />
+          <Route
+            path="/seller/message-center"
+            element={<SellersMessageCenter />}
+          />
           <Route path="/seller/orders" element={<SellersOrder />} />
           <Route path="/seller/order-info" element={<SellersOrderInfo />} />
           <Route path="/seller/products" element={<SellersProducts />} />
@@ -72,7 +90,10 @@ function App() {
           <Route path="/seller/settings" element={<SellersSettings />} />
           <Route path="/seller/rfqs-info" element={<SellersRfqInfo />} />
           <Route path="/seller/rfqs-quote" element={<SellersRfqsQuote />} />
-          <Route path="/seller/productdescription" element={<SellersProductDescription />} />
+          <Route
+            path="/seller/productdescription"
+            element={<SellersProductDescription />}
+          />
         </Routes>
       </Router>
     </>
--- src/pages/SellersLandingPage/SellersLandingPage.js
+++ src/pages/SellersLandingPage/SellersLandingPage.js
@@ -1,13 +1,277 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import Button from "react-bootstrap/Button";
+import sellerlanding1 from "../../assets/img/sellerlanding1.png";
+import sellerlanding2 from "../../assets/img/sellerlanding2.png";
+import sellerlanding3 from "../../assets/img/sellerlanding3.png";
+import supplier1 from "../../assets/img/supplier1.png";
+import supplier2 from "../../assets/img/supplier2.png";
+import supplier3 from "../../assets/img/supplier3.png";
+import "./SellersLandingPage.css";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { AiOutlineCheck } from "react-icons/ai";
+import { MdDownloadForOffline } from "react-icons/md";
+import { AiOutlinePlayCircle } from "react-icons/ai";
+import { FaTimes } from "react-icons/fa";
+import nicee1 from "../../assets/img/nicee1.png";
 
 const SellersLandingPage = () => {
   return (
     <>
       <Header />
-      <div>SellersLandingPage</div>
-      <Footer />
+      <div>
+        <div className="seller-landing-container">
+          <div className="seller-landing-register">
+            <div className="seller-landing-connect">
+              <h2>
+                <span>Connect and Trade</span> With Suppliers of Food and
+                Agriculture
+              </h2>
+
+              <p>
+                Take your business to the next level by becoming a seller on the
+                supplier’s marketplace.
+              </p>
+              <Button className="btn btn-success">Register Now</Button>
+            </div>
+
+            <div className="seller-landing-images">
+              <div className="Sld-sideimageone">
+                <img src={sellerlanding1} alt="" className="sellerlanding1" />
+              </div>
+              <div className="Sld-sideimagetwo">
+                <img src={sellerlanding2} alt="" className="sellerlanding2" />
+                <img src={sellerlanding3} alt="" className="sellerlanding3" />
+              </div>
+            </div>
+          </div>
+          <div className="why-sell">
+            <div className="Why-sell-title">
+              <h2>Why Sell on MarketPlace</h2>
+              <p>
+                Learn the basics, Here's what you need to know to start selling.
+              </p>
+            </div>
+
+            <div className="Why-sell-cards">
+              <div className="Why-sell-card">
+                <p className="why-sell-number">1</p>
+                <h5>List your item</h5>
+                <p className="Why-sell-text">
+                  You can list new products and pay affordable fees. Learn more
+                  about our pricing plans here.
+                </p>
+              </div>
+              <div className="Why-sell-card">
+                <p className="why-sell-number">2</p>
+                <h5>Get seller protection</h5>
+                <p className="Why-sell-text">
+                  We ensure that all buyers are both credible and legitimate .
+                  This ensures that all deals carried out on the platform are
+                  secure and reliable.
+                </p>
+              </div>
+              <div className="Why-sell-card">
+                <p className="why-sell-number">3</p>
+                <h5>List your item</h5>
+                <p className="Why-sell-text">
+                  You can list new products and pay affordable fees. Learn more
+                  about our pricing plans here.
+                </p>
+              </div>
+              <div className="Why-sell-card">
+                <p className="why-sell-number">4</p>
+                <h5>Promotions and marketing</h5>
+                <p className="Why-sell-text">
+                  You can Benefit from our promotions and marketing get
+                  visibility from our campaigns/promotions and insight on best
+                  selling products.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="become-supplier">
+            <h2>Want To Become A Supplier?</h2>
+            <div className="become-supplier-container">
+              <div className="Supplier-image">
+                <img src={supplier1} alt="" className="supplyimage1" />
+                <img src={supplier3} alt="" className="supplyimage2" />
+                <img src={supplier2} alt="" className="supplyimage3" />
+              </div>
+              <div className="Supplier-steps">
+                <ul>
+                  <li>
+                    <p>
+                      Step 1: Register under 5 minutes: fill the registration
+                      form and submit the required documents
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Step 2: Become an Agro-ecommerce expert: complete the
+                      dedicated new seller onboarding video & activate your
+                      seller center account to manage your shop Step
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Step 3: List your products and sell: upload products and
+                      start selling by exposing your product to a wide african
+                      client base on the marketplace.
+                    </p>
+                  </li>
+                </ul>
+                <div className="become-supplier-button">
+                  <Button>Register Now</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="plans-pricing">
+            <div className="plans-pricing-title">
+              <h2>Plans & Pricing</h2>
+              <p>
+                Simple Pricing. No hidden Feee. Advance features for your
+                business
+              </p>
+            </div>
+            <div className="plans">
+              <div className="plan-types">
+                <div className="Basic-plan">
+                  <AiOutlineLoading3Quarters style={{ color: "blue" }} />
+                  <h6>Basic Plan</h6>
+                  <p>The perfect Plan for starters</p>
+                  <h5>FREE</h5>
+                  <p>No cost on this plan</p>
+                  <div className="plans-button">
+                    <Button>Register Now</Button>
+                  </div>
+                  <div className="plans-details">
+                    <div className="plan-destails-item">
+                      <FaTimes style={{ color: "red" }} />
+                      <p>No Quote Posting</p>
+                    </div>
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>Commission: 5%</p>
+                    </div>
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>Number of Products: 5</p>
+                    </div>
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>Number of Pictures: 10</p>
+                    </div>
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>
+                        Access buyer/Seller contact details: 2$ Per contract
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="premium-plan">
+                  <AiOutlineLoading3Quarters style={{ color: "blue" }} />
+                  <h6>Premium Plan</h6>
+                  <p>The perfect Plan for Ease</p>
+                  <h5>
+                    $40 <span>/Month</span> - $50<span>/Quater</span>
+                  </h5>
+                  <p>Billed per month or Quaterly</p>
+                  <div className="plans-button">
+                    <Button>Try This plan</Button>
+                  </div>
+                  <div className="plans-details">
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>No Quote Posting</p>
+                    </div>
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>Commission: 3.5%</p>
+                    </div>
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>Number of Products: Unlimited</p>
+                    </div>
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>Number of Pictures: Unlimited</p>
+                    </div>
+                    <div className="plan-destails-item">
+                      <AiOutlineCheck />
+                      <p>Access buyer/Seller contact details: FREE</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="plan-text">
+                <p>
+                  *A Free Plan is granted to sellers after signing up. All
+                  though it is advised to pay for the Premium subscription
+                  immediately because the free plan limits the features that can
+                  be accessed greatly.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="compliane-exclusion">
+            <div className="prooduct-exclusion-container">
+              <h3>Our Supplier Complainces</h3>
+              <div className="product-exclusion">
+                <div className="product-exclusion-text">
+                  <p>
+                    All compliances on the suppliers marketplace must be adhered
+                    to. Failure to do so will be considered as a breach in our
+                    agreements and sanctions will be imposed. Kindly view the
+                    document below.
+                  </p>
+                </div>
+              </div>
+              <div className="download-pdf">
+                <MdDownloadForOffline style={{ color: "#979797" }} />
+                <p>Download the Supplier Complainces.pdf</p>
+              </div>
+            </div>
+
+            <div className="prooduct-exclusion-container">
+              <h3>Our Product Exclusions</h3>
+              <div className="product-exclusion">
+                <div className="product-exclusion-text">
+                  <p>
+                    Certain products are prohibited from being listed on the
+                    suppliers marketplace. Kindly view the document of excluded
+                    products in order to be made aware and abstain from such
+                    acts.
+                  </p>
+                </div>
+              </div>
+              <div className="download-pdf">
+                <MdDownloadForOffline style={{ color: "#979797" }} />
+                <p>Download the Product Exclusions.pdf</p>
+              </div>
+            </div>
+          </div>
+          <div className="right-marketplace">
+            <div className="plans-pricing-title">
+              <h2>Use the supplier’s Marketplace the right way</h2>
+              <p>
+                Learn the essential ways to successfully maximize the
+                marketplace for a successful trade.
+              </p>
+            </div>
+            <div className="right-marketplace-image">
+              <img src={nicee1} alt="..." />
+              <Button>
+                <AiOutlinePlayCircle size={20} /> Play Video Tutorial
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer className="seller-landing-footer" />
     </>
   );
 };
